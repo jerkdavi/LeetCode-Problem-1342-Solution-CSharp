831357e9b45b81a107569753b077f0fc423b8853
David Jerković <jerkdavi@gmail.com>
2024-06-28 13:48:50 +0200

    Commit of LeetCode Problem 1342. Solution

---
+++ NumberOfStepsToReduceANumberToZero/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumberOfStepsToReduceANumberToZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1342.
+            //int num = 14;
+            //int num = 8;
+            //int num = 123;
+            //int num = 0;
+
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int numberOfSteps = Solution.NumberOfSteps(num);
+
+            Console.WriteLine(numberOfSteps);
+
+            Console.WriteLine("Done processing.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NumberOfStepsToReduceANumberToZero/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberOfStepsToReduceANumberToZero
+{
+    public class Solution
+    {
+        public static int NumberOfSteps(int num)
+        {
+            int output = 0;
+            while (num != 0)
+            {
+                if (num % 2 == 0) { num /= 2; }
+                else { num--; }
+                output ++;
+            }
+            return output;
+        }
+    }
+}
